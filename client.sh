baf40b35f724cbdcc77c088c744023367f1f31f8
Yurii <no@email.xxx>
2025-08-16 16:34:00 +0300

    Escape double quotes in stdin

--- client.sh
+++ client.sh
@@ -5,7 +5,7 @@
 function send-telegram-message() {
     local message="$1"
     [ $# -lt 1 ] && {
-        local message="<pre>$(cat)</pre>"
+        local message="<pre>$(cat | sed 's:":\\":g')</pre>"
     }
     local result=$(curl -sd '{"chat_id": "'"$TELEGRAM_BOT_CHAT"'", "text": "'"$message"'", "parse_mode": "html"}' -H 'Content-Type: application/json' "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage")
     echo "$result" | grep '"ok":true' >/dev/null || {
